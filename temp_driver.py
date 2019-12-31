e1503ca003044867da65ddf48125d8de7cdc2a9f
Troy Chibbaro <59275339+troychibbs@users.noreply.github.com>
2019-12-31 15:32:02 -0500

    Update temp_driver.py
    Added comment informing user you can edit the imported module "sense_emu" to "sense_hat" if you're using the physical SenseHat with your raspberry pi.

--- temp_driver.py
+++ temp_driver.py
@@ -2,24 +2,27 @@ from sense_emu import SenseHat
 from time import sleep
 import csv
 from datetime import datetime
-
-date = datetime.now()
-formatted_date = date.strftime("%m_%d_%Y")
-
-read_count = 0
+# You can change sense_emu to sense_hat for physical SenseHat hardware use
 sense = SenseHat()
 
-#returns int temp as string
+#method to return double temp as string
 def get_temp_string ():
     temp = sense.get_temperature()
     return str(temp)
 
+#Formats date to set as file name
+date = datetime.now()
+formatted_date = date.strftime("%m_%d_%Y")
+
 #open file to write temp data
 with open(formatted_date + '_readings' + '.csv', mode = 'w') as temp_file:
+    
     #Create csvwriter object and write header
     temp_writer = csv.writer(temp_file, delimiter = ',', quotechar = "'", quoting = csv.QUOTE_MINIMAL)
     temp_writer.writerow(["Temperature", "Time"])
     
+    #iterates for 5 readings
+    read_count = 0
     while read_count != 5:
         
         #retrieve temp and increment count
@@ -37,6 +40,7 @@ with open(formatted_date + '_readings' + '.csv', mode = 'w') as temp_file:
         
         #freeze loop for 2 seconds
         sleep(2)
+        
 temp_file.close()
 
 
